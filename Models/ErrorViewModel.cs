9807b16f0afca915f52427f8218d7b44f0fb2ffe
Antonio Toriz <torizantonio15@gmail.com>
2021-09-10 00:08:45 -0700

    created custom validations

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,4 +1,4 @@
-using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CSMVC.Models
 {
@@ -7,5 +7,77 @@ namespace CSMVC.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+    public class ContainsLetter : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Password required.");
+            }
+
+            string letter = @"abcdefghijklmnopqrstuvwxyz";
+            foreach (char item in letter)
+            {
+                foreach(char ch in (string)value)
+                {
+                    if (ch==item)
+                    {
+                        return ValidationResult.Success;
+                    }
+                }
+            }
+            return new ValidationResult("Password must contain at least one letter.");
+        }
+    }
+
+    public class ContainsNumber : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Password required.");
+            }
+
+            string numbers = @"0123456789";
+            foreach (char num in numbers)
+            {
+                foreach(char ch in (string)value)
+                {
+                    if (ch == num)
+                    {
+                        return ValidationResult.Success;
+                    }
+                }
+            }
+            return new ValidationResult("Password must contain at least one number");
+        }
+    }
+
+    public class ContainsSpecialCharacter : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Password required.");
+            }
+
+            string specialChar = @"\|!#$%&/()=?»«@£§€{}.-;'<>_,";
+            foreach (char spch in specialChar)
+            {
+                foreach(char ch in (string)value)
+                {
+                    if (ch == spch)
+                    {
+                        return ValidationResult.Success;
+                    }
+                }
+            }
+            return new ValidationResult("Password must contain at least one special character.");
+        }
     }
 }
